8f4c96c9d846fa894aea4f2ca60590896bdf7c47
PietroSavini <savinipietro@gmail.com>
2023-05-16 18:32:48 +0200

    BONUS 1 Completato con OGG "Genre"

--- index.php
+++ index.php
@@ -19,14 +19,25 @@ class Movie {
         echo 'titolo: ' . $this->title . "<br>";
         echo 'durata: ' . $this->duration . "<br>";
         echo 'descrizione: ' . $this->description . "<br>";
-        echo 'generi: ' . implode(", ",$this->genre) . "<br>";
+        echo 'generi: ' . implode(", ",$this->genre->genere) . "<br>";
     }
 
 }
 
-$first_movie = new Movie(["comico","demenziale"], "80 min", "Una Notte Da Leoni", "un gruppo di ragazzi ormai alle prese con l'età adulta passeranno un addio al nubilato speciale.");
+// ho creato l ogetto Genre per ovviare al fatto che se si creasse un film con un solo genere senza array il genere non verrebbe messo a display
+class Genre {
+    //attributi
+    public $genere;
 
-$second_movie = new Movie(["horror","splatter"], "80 min", "Non Aprite quella Porta", "uno che sta poco bene mentalmente avendo la famiglia altrettanto malata ora si diletta a scassare la gente e rubargli la faccia")
+    function __construct($_genres)
+    {
+        $this->genere = $_genres;
+    }
+}
+
+$first_movie = new Movie( new Genre(["demenziale","comico","inarrestabile"]), "80 min", "Una Notte Da Leoni", "un gruppo di ragazzi ormai alle prese con l'età adulta passeranno un addio al nubilato speciale.");
+
+$second_movie = new Movie(new Genre(["horror"]), "80 min", "Non Aprite quella Porta", "uno che sta poco bene mentalmente avendo la famiglia altrettanto malata ora si diletta a scassare la gente e rubargli la faccia")
 ?>
 
 <!DOCTYPE html>
@@ -39,9 +50,9 @@ $second_movie = new Movie(["horror","splatter"], "80 min", "Non Aprite quella Po
 </head>
 <body>
     <h2>primo film</h2>
-    <?php $first_movie->moviePrint() ?>
+    <?php echo $first_movie->moviePrint() ?>
 
     <h2>secondo film</h2>
-    <?php $second_movie->moviePrint() ?>
+    <?php echo $second_movie->moviePrint() ?>
 </body>
 </html>
