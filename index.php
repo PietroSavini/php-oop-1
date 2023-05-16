82083625d091d5fa4f5033d94bd44d699bc8dc83
PietroSavini <savinipietro@gmail.com>
2023-05-16 17:50:57 +0200

    CONSEGNA : NESSUN BONUS

--- index.php
+++ index.php
@@ -6,13 +6,27 @@ class Movie {
     public $title;
     public $description;
 
+    //costruttore
     function __construct ($_genre, $_duration, $_title, $_description){
         $this->genre = $_genre;                
         $this->duration = $_duration;
         $this->title = $_title;
         $this->description = $_description;
     }
+
+    //methods
+    public function moviePrint(){
+        echo 'titolo:' . $this->title . "<br>";
+        echo 'durata:' . $this->duration . "<br>";
+        echo 'descrizione:' . $this->description . "<br>";
+        echo 'generi:' . $this->genre . "<br>";
+    }
+
 }
+
+$first_movie = new Movie("comico", "80 min", "Una Notte Da Leoni", "un gruppo di ragazzi ormai alle prese con l'età adulta passeranno un addio al nubilato speciale.");
+
+$second_movie = new Movie("horror", "80 min", "Non Aprite quella Porta", "uno che sta poco bene mentalmente avendo la famiglia altrettanto malata ora si diletta a scassare la gente e rubargli la faccia")
 ?>
 
 <!DOCTYPE html>
@@ -24,6 +38,10 @@ class Movie {
     <title>Document</title>
 </head>
 <body>
-    
+    <h2>primo film</h2>
+    <?php $first_movie->moviePrint() ?>
+
+    <h2>secondo film</h2>
+    <?php $second_movie->moviePrint() ?>
 </body>
 </html>
